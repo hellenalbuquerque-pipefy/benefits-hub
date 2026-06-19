d8dbf92201902a3c3c36c3ec1df46e3b3ca61c03
Hellen <hellen.albuquerque@pipefy.com>
2026-06-19 14:55:58 -0300

    Ajuste parental guide

--- js/components.js
+++ js/components.js
@@ -28,7 +28,7 @@ function renderHeader() {
 
   const navLinks = us ? `
       <a href="${BASE}us/index.html">Home</a>
-      <a href="${BASE}us/parental-leave.html">Parental Leave</a>
+      <a href="${BASE}us/parental-guide.html">Parental Guide</a>
       <a href="${BASE}us/useful-links.html">Useful Links</a>
       <a href="${BASE}us/contact.html" class="nav-cta">Contact the Team</a>` : `
       <a href="${BASE}index.html">Home</a>
